bea6f787ea803c2bf856376561efbfe89e04616c
Steven <i.am.lucky.s.13@gmail.com>
2024-07-26 01:51:40 +0300

    Added some variables. Lesson_1/Tesk1

--- Lesson_1/Task1/Program.cs
+++ Lesson_1/Task1/Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Hello world");
+﻿// Console.WriteLine("Hello world");
+
+
+int value = 100;
+Console.WriteLine(value);
+
+double d = 5.55;
+char c = '1';
+bool b = true;
+Console.WriteLine(b);
+Console.WriteLine(c);
+Console.WriteLine(d);
