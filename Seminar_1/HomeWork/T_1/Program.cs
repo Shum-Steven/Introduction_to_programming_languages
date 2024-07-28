85a4f99b862b532cd488187f1bb741a239f29c15
Steven <i.am.lucky.s.13@gmail.com>
2024-07-29 01:52:12 +0300

    Completed T_1

---
+++ Seminar_1/HomeWork/T_1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Внутри класса Answer напишите метод CompareNumbers, 
+// который на вход принимает два числа и выводит, какое число большее, а какое меньшее:
+
+byte firstNumber = 3;
+byte secondNumber = 3;
+
+if (firstNumber >= secondNumber)
+{
+    Console.WriteLine(firstNumber);
+}
+else
+{
+    Console.WriteLine(secondNumber);
+}
