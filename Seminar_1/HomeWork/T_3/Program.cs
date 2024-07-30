26f9af76fdb18c27bf681fea1b12bb895a8c57ae
Steven <i.am.lucky.s.13@gmail.com>
2024-07-30 20:09:45 +0300

    Completed T_3.

---
+++ Seminar_1/HomeWork/T_3/Program.cs
@@ -0,0 +1,18 @@
+﻿// Внутри класса Answer напишите метод CheckIfEven, который на вход 
+// принимает число number и выводит, является ли число чётным (делится ли оно на два без остатка).
+
+Console.WriteLine("введите Перовое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+// int number = 6;
+
+// int number = 3;
+
+if (number % 2 == 0)
+{
+    Console.WriteLine("Число чётное ");
+}
+if (number % 2 == 1)
+{
+    Console.WriteLine("Число нечётное ");
+}
