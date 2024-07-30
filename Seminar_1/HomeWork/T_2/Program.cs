6ced0b86e0fe569a4e4446ee0701743b8c886ef9
Steven <i.am.lucky.s.13@gmail.com>
2024-07-30 18:26:18 +0300

    Completed T_2.

---
+++ Seminar_1/HomeWork/T_2/Program.cs
@@ -0,0 +1,27 @@
+﻿// Внутри класса Answer напишите метод FindMax, который 
+// принимает на вход три числа и выдаёт маскимальное из этих чисел.
+
+Console.WriteLine("введите Перовое число: ");
+int a = Convert.ToInt16(Console.ReadLine());
+
+Console.WriteLine("введите Второе число: ");
+int b = Convert.ToInt16(Console.ReadLine());
+
+Console.WriteLine("введите Третье число: ");
+int c = Convert.ToInt16(Console.ReadLine());
+
+// int a = 7;
+// int b = 137;
+// int c = 13;
+
+int MAX = a;
+
+if (b > MAX)
+{
+    MAX = b;
+}
+if (c > MAX)
+{
+    MAX = c;
+}
+Console.WriteLine(MAX);
