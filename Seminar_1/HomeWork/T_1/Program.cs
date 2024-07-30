4896b4dc83de65c53b071c88b621b3e135578bc6
Steven <i.am.lucky.s.13@gmail.com>
2024-07-30 14:16:17 +0300

    Added input of numbers from the user.

--- Seminar_1/HomeWork/T_1/Program.cs
+++ Seminar_1/HomeWork/T_1/Program.cs
@@ -1,8 +1,20 @@
 ﻿// Внутри класса Answer напишите метод CompareNumbers, 
 // который на вход принимает два числа и выводит, какое число большее, а какое меньшее:
 
-byte firstNumber = 3;
-byte secondNumber = 3;
+Console.WriteLine("Введите перовое число: ");
+int firstNumber = Convert.ToInt16(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int secondNumber = int.Parse(Console.ReadLine());
+
+// byte firstNumber = 5;
+// byte secondNumber = 6;
+
+// byte firstNumber = 6;
+// byte secondNumber = 3;
+
+// byte firstNumber = 3;
+// byte secondNumber = 3;
 
 if (firstNumber >= secondNumber)
 {
--- Seminar_1/HomeWork/T_1/obj/Debug/net8.0/T_1.AssemblyInfo.cs
+++ Seminar_1/HomeWork/T_1/obj/Debug/net8.0/T_1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("T_1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3727d8da27afc629ad8e596b8b6a8058e6904100")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85a4f99b862b532cd488187f1bb741a239f29c15")]
 [assembly: System.Reflection.AssemblyProductAttribute("T_1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("T_1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
