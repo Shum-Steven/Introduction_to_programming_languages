ada02207477c596008c34533b888cde0d6069bc1
Steven <i.am.lucky.s.13@gmail.com>
2024-08-01 02:33:07 +0300

    Completed Task_4 of Semainar_1/part_2

---
+++ Seminar_1/Task_7/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая выводит третью с конца цифру заданного
+// числа или сообщает, что третей цифры нет.
+// 456 => 6
+// 7812 => 8
+// 91 => Третей цифры нет
+
+System.Console.WriteLine("введите Число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number > 99)
+{
+    System.Console.WriteLine(number / 100 % 10);
+    // System.Console.WriteLine(number % 1000 / 100);
+}
+else
+{
+    System.Console.WriteLine("Нет Третьей Цифры!");
+}
