b8b945eabcb56c6bbbe168894b0e49b0c3eaa510
Steven <i.am.lucky.s.13@gmail.com>
2024-07-31 16:05:20 +0300

    Completed Task_1 of Seminar_1/part_2

---
+++ Seminar_1/Task_4/Program.cs
@@ -0,0 +1,55 @@
+﻿// Напишите программу которая на вход принимает трёхзначное число
+// и удаляет вторую цифру этогоч сила.
+// Примеры:
+// a = 256 => 26
+// a = 891 => 81
+
+ System.Console.WriteLine("введите Трёхзначное число: ");
+ int number = Convert.ToInt32(Console.ReadLine());
+
+// string str1 = "key";
+// string str2 = "word";
+// // && - логическое И
+// // || - логическое Или
+// if (str1 == "key" && str2 == "word")
+// {
+//     System.Console.WriteLine("Да");
+// }
+// else
+// {
+//     System.Console.WriteLine("Нет");
+// }
+
+if(number > 99 && number < 1000)
+{
+    int leftDigit = number / 100;
+    int rightDigit = number % 10;
+    int result = leftDigit * 10 + rightDigit;
+    System.Console.WriteLine(result);
+}
+else
+{
+    System.Console.WriteLine("число НеТрёхзначное!!!");
+}
+
+// if(number > 99 && number < 1000)
+// {
+//     int leftDigit = number / 100;
+//     int rightDigit = number % 10;
+//     string result = leftDigit + "" + rightDigit;
+//     int integerResult = Convert.ToInt32(result);
+//     System.Console.WriteLine(integerResult);
+// }
+// else
+// {
+//     System.Console.WriteLine("число НеТрёхзначное!!!");
+// }
+
+// if(number > 99 && number < 1000)
+// {
+//     System.Console.WriteLine(number /100 + "" + number % 10);
+// }
+// else
+// {
+//     System.Console.WriteLine("число НеТрёхзначное!!!");
+// }
