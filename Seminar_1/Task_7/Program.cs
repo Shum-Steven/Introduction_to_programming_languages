7dc00a78d6b3c5310c09528e91975e7e14824fa6
Steven <i.am.lucky.s.13@gmail.com>
2024-08-01 11:49:11 +0300

    Added a solution Task_4 of Semainar_1/part_4

--- Seminar_1/Task_7/Program.cs
+++ Seminar_1/Task_7/Program.cs
@@ -7,12 +7,25 @@
 System.Console.WriteLine("введите Число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
+// if(number > 99)
+// {
+//     System.Console.WriteLine(number / 100 % 10);
+//     // System.Console.WriteLine(number % 1000 / 100);
+// }
+// else
+// {
+//     System.Console.WriteLine("Нет Третьей Цифры!");
+// }
+
+// 123567 / 10 -> 12356 / 10 -> 1235 / 10 -> 123 % 10 -> 3
 if(number > 99)
 {
-    System.Console.WriteLine(number / 100 % 10);
-    // System.Console.WriteLine(number % 1000 / 100);
+    while(number > 999)
+        number /= 10; // number = number / 10
+
+    System.Console.WriteLine(number % 10);
 }
 else
-{
+{     
     System.Console.WriteLine("Нет Третьей Цифры!");
 }
--- Seminar_1/Task_7/obj/Debug/net8.0/Task_7.AssemblyInfo.cs
+++ Seminar_1/Task_7/obj/Debug/net8.0/Task_7.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task_7")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d523490e569f6d354d3d03aac932f5602e1dcb8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ada02207477c596008c34533b888cde0d6069bc1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task_7")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task_7")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
