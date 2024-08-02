928bb55155da9bf74c03d0aacc521b7f3452cf69
Steven <i.am.lucky.s.13@gmail.com>
2024-08-02 09:47:20 +0300

    Completed HomeWork from Lesson_1

---
+++ Seminar_1/HomeWork/T_5/Program.cs
@@ -0,0 +1,17 @@
+﻿// Вычеслите значение формулы a * b / (c + d),
+// где a, b, c, d - некоторые целые числа. Результат вывести на экран.
+
+System.Console.WriteLine(" введите число для значения a : ");
+double a = Convert.ToDouble(Console.ReadLine());
+
+System.Console.WriteLine(" введите число для значения b : ");
+double b = Convert.ToDouble(Console.ReadLine());
+
+System.Console.WriteLine(" введите число для значения c : ");
+double c = Convert.ToDouble(Console.ReadLine());
+
+System.Console.WriteLine(" введите число для значения d : ");
+double d = Convert.ToDouble(Console.ReadLine());
+
+double result = a * b / (c + d);
+System.Console.Write(result );
