293c3d026ebef23ea16182ddcd5c1bad7565a715
Steven <i.am.lucky.s.13@gmail.com>
2024-08-02 09:11:48 +0300

    Completed Task_4 for Seminar_1/part_2

---
+++ Seminar_1/HomeWork/T_9/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая на вход принимает натуральное число N,
+// а на выходе показывает его цифры через запятую.
+
+System.Console.WriteLine(" введите Натуральное Число ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+if(N > 0)
+{
+    while(N > 0)
+    {
+    int result = N % 10;
+    System.Console.Write($"{result}, ");
+    N /= 10;
+    }
+}
+else
+{
+    System.Console.WriteLine("значение не соотвествует условию");
+}
