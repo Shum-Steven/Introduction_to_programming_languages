632075f322685947da8b4d9e38ef280d70c0b285
Steven <i.am.lucky.s.13@gmail.com>
2024-08-13 08:22:26 +0300

    Add Seminar_2/Task2

---
+++ Seminar_2/Task2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задайте массив из 10 элементов, заполненый числами из промежутка [-10, 10].
+// Заменительные отрицательные элементы на положительные,
+// а положительные на отрицательные.
+// Пример:
+//  [1 -5 6]
+//  => [-1 5 -6]
+
+int[] numbers = { 1, -5, 6, 6, 3, 9, -7 };
+
+foreach (int number in numbers)
+{
+    System.Console.Write(number + "\t");
+}
+System.Console.WriteLine();
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    numbers[i] *= -1;
+}
+
+foreach(int number in numbers)
+{
+    System.Console.Write(number + "\t");
+}
