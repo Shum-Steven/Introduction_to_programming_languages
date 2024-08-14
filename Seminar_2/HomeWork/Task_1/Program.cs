fd8b97849ac2a3f5a2536804141fc3114e8ffb25
Steven <i.am.lucky.s.13@gmail.com>
2024-08-14 11:43:36 +0300

    Add S_2/Hw/Task_1

---
+++ Seminar_2/HomeWork/Task_1/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задайте одномерный массив из 10 целых чисел от 1 до 100.
+// Найдите количество элементов массива, значения которых
+//  лежит в отрезке [20, 90].
+// Пример:
+//  массив [10 21 14 93 23] => 2
+
+int[] array = { 10, 21, 14, 93, 23 };
+
+// int i = 0;
+// int counter = 0;
+
+// while (i < array.Length)
+// {
+//     if (array[i] >= 20 && array[i] <= 90)
+//     {
+//         counter = counter + 1;
+//     }
+//     i++;
+// }
+// System.Console.WriteLine(" => " + counter);
+
+// for (int i = 0; i < array.Length; i++)
+// {
+//     if (array[i] >= 20 && array[i] <= 90)
+//         counter = counter + 1;
+// }
+// Console.WriteLine($" => {counter}");
+
+foreach (int number in array)
+{
+    if (number >= 20 && number <= 90)
+        Console.Write(number + "\t");
+}
