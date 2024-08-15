4bd49cbc7fc7d5b8a97db43b68e41406e956c323
Steven <i.am.lucky.s.13@gmail.com>
2024-08-15 08:43:57 +0300

    Seminar_3/Task1

---
+++ Seminar_3/Task1/Program.cs
@@ -0,0 +1,28 @@
+﻿// тип_возвращаемого_значения   _имя_функции_(принимаемые_аргументы)
+
+void Main()
+{
+    int a = 7;
+    int b = 8;
+    int c = 13;
+
+    int d = 10;
+    int e = 5;
+    int f = 3;
+
+    int result = Function(a, b, c);
+    System.Console.WriteLine(result);
+
+    int result2 = Function(d, e, f);
+    System.Console.WriteLine(result2);
+}
+
+// тип_возвращаемого_значения   _имя_функции_(принимаемые_аргументы)
+//                     7            18                  13
+int Function(int firstValue, int secondValue, int thirdValue)
+{
+    int result = (firstValue + secondValue - thirdValue) * 2 + 3 * secondValue - 10;
+    return result;
+}
+
+Main();
