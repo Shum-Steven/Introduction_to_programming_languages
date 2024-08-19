11d444bc34a5468d0fa9b10acc9c4a290ca03fbc
Steven <i.am.lucky.s.13@gmail.com>
2024-08-20 02:52:14 +0300

    Add Lesson_4/Task3

---
+++ Lesson_4/Task3/Program.cs
@@ -0,0 +1,16 @@
+﻿string GetLetters(string s)
+{
+    string letters = " ";
+    foreach(char e in s)
+    {
+        if(char.IsAsciiLetter(e) == true)
+        {
+            letters = letters + e;
+        }
+    }
+    return letters;
+}
+
+string str = Console.ReadLine();
+string resalt = GetLetters(str);
+Console.Write(resalt);
