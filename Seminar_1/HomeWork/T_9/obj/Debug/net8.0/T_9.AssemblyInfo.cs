f8821be86273dd50635a44a6af0bf986264c38c7
Steven <i.am.lucky.s.13@gmail.com>
2024-08-19 03:22:24 +0300

    Fixed Seminar_1

--- Seminar_1/HomeWork/T_9/obj/Debug/net8.0/T_9.AssemblyInfo.cs
+++ Seminar_1/HomeWork/T_9/obj/Debug/net8.0/T_9.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("T_9")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41db578698cd2734c50e68cad28ac04616f0508f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a887b96285daf8708b7e06d387daccbdba90c2b")]
 [assembly: System.Reflection.AssemblyProductAttribute("T_9")]
 [assembly: System.Reflection.AssemblyTitleAttribute("T_9")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
