33c7497191cb7729bce83653aa8404baa93b6145
Steven <i.am.lucky.s.13@gmail.com>
2024-08-19 12:32:12 +0300

    Add Homework

---
+++ Seminar_3/HomeWork/Task_2/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задайте массив на 10 целых чисел. Напишите программу,
+// которая определяет количество чётных чисел в массиве.
+// Пример:
+// массив [6 7 19 34 3 1 4 7 9 1] => 3
+// массив [1 8 43 4 55 60 3 2 1 3] => 4
+
+int[] array = { 2, 5, 2, 3, 6, 3, 7, 8, 2, 4 };
+// int i = 0;
+int count = 0;
+
+// while (i < array.Length)
+// {
+//     if (array[i] % 2 == 0)
+//     {
+//         count = count + 1;
+//     }
+//     i++;
+// }
+// System.Console.WriteLine(" => " + count);
+
+foreach (int number in array)
+{
+    if (number % 2 == 0)
+        Console.Write(number + "\t");
+}
+System.Console.WriteLine();
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] % 2 == 0)
+        count = count + 1;
+}
+Console.WriteLine($" => {count}");
