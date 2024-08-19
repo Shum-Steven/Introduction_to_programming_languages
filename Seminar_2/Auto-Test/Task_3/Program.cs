5ba7ac7335b930b8f6ef2b3280ae02bf7121641a
Steven <i.am.lucky.s.13@gmail.com>
2024-08-19 14:05:05 +0300

    Auto-Test Done Task_3

---
+++ Seminar_2/Auto-Test/Task_3/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задайте массив из 10 целых чисел.
+// Найдите среднее значение элементов массива. 
+
+int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+int sum = 0;
+
+for(int i = 0; i < array.Length; i++)
+{
+    sum += array[i]; // sum = sum + array[i];
+}
+
+double avr = sum / array.Length;
+System.Console.WriteLine(avr);
