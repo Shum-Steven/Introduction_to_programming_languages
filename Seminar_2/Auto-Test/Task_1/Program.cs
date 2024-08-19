1c5f8c267adf5ed286b1aba3dda8d25587d5c6da
Steven <i.am.lucky.s.13@gmail.com>
2024-08-19 13:11:54 +0300

    Auto-Test Done Task_1

--- Seminar_2/Auto-Test/Task_1/Program.cs
+++ Seminar_2/Auto-Test/Task_1/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте массив целых чисел. Напишите программу, кторая проверяет,
+// присутствует ли заданное число в массиве. Программа должна вывести:
+// Присутствует / Не присутствует
+
+int[] numbers = { 1, 3, 4, 8, 19, 3 };
+//System.Console.Write(" введите число для поиска в массиве: ");
+int numberToFind = 8; //Convert.ToInt32(Console.ReadLine());
+
+bool numberIsFinded = false;
+
+foreach (int n in numbers)
+{
+    if (n == numberToFind)
+    {
+        numberIsFinded = true;
+    } 
+}
+if(numberIsFinded)
+    {
+        System.Console.WriteLine("Присутствует");
+
+    }
+    else
+    {  
+        System.Console.WriteLine("Не присутствует");
+    }
