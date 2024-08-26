18f9b1d64d1f2cb271c95aafddd65389fb8a2da7
Shum-Steven <i.am.lucky.s.13@gmail.com>
2024-08-26 20:05:27 +0300

    Add Lesson_5/Task1

---
+++ Lesson_5/Recursion_Task1/Program.cs
@@ -0,0 +1,12 @@
+﻿int Fact(int n)
+{
+    if (n == 1 || n == 0)
+    {
+        System.Console.WriteLine($"Stop: {n}");
+        return 1;
+    }
+    System.Console.WriteLine(n);
+    return n * Fact(n - 1);
+}
+
+Console.Write(Fact(5));
