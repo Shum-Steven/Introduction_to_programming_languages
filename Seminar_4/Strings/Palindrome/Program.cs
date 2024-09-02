77080c19ba5215d63384bbe98e2cb3aafb0958c8
Shum-Steven <i.am.lucky.s.13@gmail.com>
2024-09-02 22:20:52 +0300

    Complete Seminar_4/part2/HomeWork_Palindrom

---
+++ Seminar_4/Strings/Palindrome/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задайте произвольную строку.
+// Выясните, является ли она палиндромом
+//     Пример:
+// "aBcD1ef!-" => Нет
+// "шалаш" => Да
+// "55655" => Да
+
+System.Console.Write("введите Палиндром : ");
+string? str = Console.ReadLine();
+char[] palindrom = new char[str!.Length];
+
+for(int i = 0; i < str.Length; i++)
+{
+    palindrom[i] = str[i];
+}
+
+int j = 0;
+while(j < palindrom.Length)
+{
+    if (palindrom[j] == palindrom[palindrom.Length - 1 - j])
+    {
+        j++;
+    }
+    else
+    {
+        System.Console.Write(" => Нет ");
+        return;
+    }
+}
+System.Console.Write(" => Да ");
